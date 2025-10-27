058866412d984c3dd87528bc12932b48bb84dd16
Sacripeace <jenuks@daum.net>
2025-10-27 15:43:38 +0900

    FIX: App.js 수정사항 커밋

--- src/App.js
+++ src/App.js
@@ -3,24 +3,17 @@ import './css/App.css';
 import './css/reset.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-<<<<<<< HEAD
-=======
-
->>>>>>> 930a6c9 (1.1.0_251027_final)
 import Academy from './pages/Academy';
 import TeacherRegister from './pages/TeacherRegister';
 import Login from './pages/Login';
 import Homepage from './pages/Homepage';
 import AcademyRegister from './pages/AcademyRegister';
-<<<<<<< HEAD
-=======
 import FindMyId from './pages/FindMyId';
 import Repassword from './pages/RePassword';
 import Signup from './pages/Signup';
 
 
 
->>>>>>> 930a6c9 (1.1.0_251027_final)
 
 
 function App() {
