25d39a3fa4112635ce4d04f2f98f84ec27edec0a
Sacripeace <jenuks@daum.net>
2025-10-27 15:48:05 +0900

    Fix수정한 login.js파일이최종파일로 예상

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,46 +12,6 @@ function Login() {
           <div className="login_outbox_01">
             <div className="login_text_01">관리자 로그인</div>
 
-<<<<<<< HEAD
-                        <div className="login_box_01">
-                            <div className="title_13px">암호</div>
-                            <input type="password" id="password" name="password" placeholder="비밀번호를를 입력하세요."/>
-                        </div>
-
-                        <div className="login_box_02">
-                            <div className="text_13px">아이디찾기</div>
-                        
-                           <button type="submit" className="login_btn">확인</button>
-                        </div>
-
-                        <div className="sns_Login">
-                            <img className="snsImg" src="./images/kakao_Login.png" alt="카카오톡 로그인"></img>
-                            <img className="snsImg" src="./images/Naver_Login.png" alt="네이버 로그인"></img>
-                        </div>
-                        
-                        <div className="signup_container_box"> 
-                            <div className="signup_box_01"> 
-                                    <div className="signup_text_01">회원이 아니세요?</div>
-                                    <div className="signup_text_02">회원가입 </div>
-                            </div>
-                    </div>
-                </div>
-
-                    <div className="container_outbox_02">
-                        <div className="slogan_logo_01">
-
-                            <img className="Logo" src="./images/naillo_logo.png" alt="로고" >
-
-                            </img>
-                            {/* http://localhost:3000/images/ naillo_logo.png */}
-                        </div>
-                    
-                        <div className="slogan_text_01">“배움을 통해 내일로 나아가는 플랫폼”</div>
-                        <div className="slogan_text_01"> Learn today, Lead Tomorrow. </div>
-                    
-                    </div>
-                </div>  
-=======
             <div className="login_box_01">
               <div className="title_13px">아이디</div>
               <input 
@@ -60,7 +20,6 @@ function Login() {
                 name="userid" 
                 placeholder="아이디를 입력하세요." 
               />
->>>>>>> 930a6c9 (1.1.0_251027_final)
             </div>
 
             <div className="login_box_01">
