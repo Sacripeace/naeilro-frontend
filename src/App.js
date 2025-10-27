f9dfddccae71846ddc72056218a3ffff02025c94
ddunho <dydkwogur@naver.com>
2025-10-27 10:49:19 +0900

    last commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer.js';
 import Academy from './pages/Academy.js';
 import Login from './pages/Login.js';
 import TeacherRegister from './pages/TeacherRegister.js';
+import AcademyRegister from './pages/AcademyRegister.js';
 
 function App() {
   return (   
@@ -16,6 +17,7 @@ function App() {
         <Route path="/academy/:uid" element={<Academy />} />
         <Route path="/teacher-register" element={<TeacherRegister />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/academy-register" element={<AcademyRegister />} />
       </Routes>
       <Footer></Footer>
     </BrowserRouter>
--- src/pages/TeacherRegister.js
+++ src/pages/TeacherRegister.js
@@ -2,83 +2,83 @@ import "../css/teacher_register.css";
 
 function TeacherRegister(){
     return(
-        <main className="body">
-            <div className="container">
-                <div className="title">
+        <main className="teacherbody">
+            <div className="teachercontainer">
+                <div className="teachertitle">
                     <p>강사 정보 등록</p>
                     <p>강사 추가+</p>
                 </div>
-                <div className="line"></div>
-                <div className="content">
+                <div className="teacherline"></div>
+                <div className="teachercontent">
                     <div>
-                        <div className="box">
-                            <div className="name">
+                        <div className="teacherbox">
+                            <div className="teachername">
                                 <p>성명</p>
                                 <input placeholder="강사님의 성명을 입력해 주세요"></input>
                             </div>
                         </div>
-                        <div className="box">
-                            <div className="name">
+                        <div className="teacherbox">
+                            <div className="teachername">
                                 <p>경력</p>
                                 <input placeholder="간단한 경력을 입력해 주세요"></input>
                             </div>
                         </div>
-                        <div className="box">
-                            <div className="name">
+                        <div className="teacherbox">
+                            <div className="teachername">
                                 <p>과목</p>
-                                <div className="checkcontainer">
+                                <div className="teachercheckcontainer">
                                     <button>게임 개발</button>
                                     <button>AI 개발</button>
                                     <button>Java</button>
                                 </div>
                             </div>
                         </div>
-                        <div className="box">
-                            <div className="name">
-                                <p className="recruit">모집 인원</p>
+                        <div className="teacherbox">
+                            <div className="teachername">
+                                <p className="teacherrecruit">모집 인원</p>
                                 <input placeholder="모집 인원 수를 입력해 주세요"></input>
                             </div>
                         </div>
                     </div>
 
-                    <div className="imageupload"><p>사진을 등록해 주세요</p></div>
+                    <div className="teacherimageupload"><p>사진을 등록해 주세요</p></div>
                 </div>
-                <div className="subline"></div>
-                <div className="content">
+                <div className="teachersubline"></div>
+                <div className="teachercontent">
                     <div>
-                        <div className="box">
-                            <div className="name">
+                        <div className="teacherbox">
+                            <div className="teachername">
                                 <p>성명</p>
                                 <input placeholder="강사님의 성명을 입력해 주세요"></input>
                             </div>
                         </div>
-                        <div className="box">
-                            <div className="name">
+                        <div className="teacherbox">
+                            <div className="teachername">
                                 <p>경력</p>
                                 <input placeholder="간단한 경력을 입력해 주세요"></input>
                             </div>
                         </div>
-                        <div className="box">
-                            <div className="name">
+                        <div className="teacherbox">
+                            <div className="teachername">
                                 <p>과목</p>
-                                <div className="checkcontainer">
+                                <div className="teachercheckcontainer">
                                     <button>게임 개발</button>
                                     <button>AI 개발</button>
                                     <button>Java</button>
                                 </div>
                             </div>
                         </div>
-                        <div className="box">
-                            <div className="name">
-                                <p className="recruit">모집 인원</p>
+                        <div className="teacherbox">
+                            <div className="teachername">
+                                <p className="teacherrecruit">모집 인원</p>
                                 <input placeholder="모집 인원 수를 입력해 주세요"></input>
                             </div>
                         </div>
                     </div>
 
-                    <div className="imageupload"><p>사진을 등록해 주세요</p></div>
+                    <div className="teacherimageupload"><p>사진을 등록해 주세요</p></div>
                 </div>
-                <div className="okcontainer">
+                <div className="teacherokcontainer">
                     <button>완료</button>
                 </div>
             </div>
